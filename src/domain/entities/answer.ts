b3214fa9ca0ba30ffced4e328c2a14bed48733b0
FIllipe Romero <fillipe.romero@gmail.com>
2023-07-28 11:56:42 -0600

    Refactor answer and question entities

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,11 +1,22 @@
 import { randomUUID } from "node:crypto"
 
+
+interface AnswerProps {
+  content: string
+  questionId: string
+  authorId: string
+}
+
 export class Answer {
   public id: string
   public content: string
+  public authorId: string
+  public questionId: string
 
-  constructor(content: string, id?: string) {
+  constructor(props: AnswerProps, id?: string) {
     this.id = id ?? randomUUID()
-    this.content = content
+    this.content = props.content
+    this.authorId = props.authorId
+    this.questionId = props.questionId
   }
 }
