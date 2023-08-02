366475c02ce6baba61c39d0e10156d8c15b46a44
FIllipe Romero <fillipe.romero@gmail.com>
2023-08-02 00:24:21 -0600

    Add factory to generate question

--- src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -1,7 +1,7 @@
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
 import { GetQuestionBySlugUseCase } from './get-question-by-slug'
-import { Question } from '../../enterprise/entities/question'
-import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { makeQuestion } from 'test/factories/make-question'
+import { Slug } from '../../enterprise/entities/value-objects/slug'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
 let sut: GetQuestionBySlugUseCase // sut => System Under Test
@@ -13,11 +13,9 @@ describe('Get Question By Slug', () => {
   })
 
   it('should be able to get a question by slug', async () => {
-    const newQuestion = Question.create({
-      title: 'Example Question',
-      authorId: new UniqueEntityID('author-id'),
-      content: 'content',
-    })
+    const newQuestion = makeQuestion({ slug: Slug.create('example-question') })
+
+    console.log(newQuestion)
 
     await inMemoryQuestionsRepository.create(newQuestion)
 
