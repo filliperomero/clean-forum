9aa57d500e47569e1b524c8241b6bfdc46bd3f8a
FIllipe Romero <fillipe.romero@gmail.com>
2023-08-02 00:02:55 -0600

    Refactor test

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,21 +1,23 @@
 import { AnswerQuestionUseCase } from './answer-question'
-import { AnswersRepository } from '../repositories/answers-repository'
-import { Answer } from '../../enterprise/entities/answer'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
 
-const fakeAnswersRepository: AnswersRepository = {
-  create: async (answer: Answer) => {
-    return undefined
-  },
-}
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: AnswerQuestionUseCase // sut => System Under Test
 
-test('create an answer', async () => {
-  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
-
-  const answer = await answerQuestion.execute({
-    instructorId: 'any-id',
-    questionId: 'any-id',
-    content: 'New Answer',
+describe('Answer Question', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    sut = new AnswerQuestionUseCase(inMemoryAnswersRepository)
   })
 
-  expect(answer.content).toEqual('New Answer')
+  it('should be able to answer a question', async () => {
+    const { answer } = await sut.execute({
+      instructorId: 'any-id',
+      questionId: 'any-id',
+      content: 'New Answer',
+    })
+
+    expect(answer.content).toEqual('New Answer')
+    expect(inMemoryAnswersRepository.items[0].id).toEqual(answer.id)
+  })
 })
--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,22 +1,24 @@
-import { QuestionsRepository } from '../repositories/questions-repository'
-import { Question } from '../../enterprise/entities/question'
 import { CreateQuestionUseCase } from './create-question'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
 
-const fakeQuestionsRepository: QuestionsRepository = {
-  create: async (question: Question) => {
-    return undefined
-  },
-}
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: CreateQuestionUseCase // sut => System Under Test
 
-test('create a question', async () => {
-  const createQuestion = new CreateQuestionUseCase(fakeQuestionsRepository)
-
-  const { question } = await createQuestion.execute({
-    authorId: 'authorId',
-    title: 'Any Title',
-    content: 'Any Content',
+describe('Create Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new CreateQuestionUseCase(inMemoryQuestionsRepository)
   })
 
-  expect(question.id).toBeTruthy()
-  expect(question.title).toEqual('Any Title')
+  it('should be able to create a question', async () => {
+    const { question } = await sut.execute({
+      authorId: 'authorId',
+      title: 'Any Title',
+      content: 'Any Content',
+    })
+
+    expect(question.id).toBeTruthy()
+    expect(question.title).toEqual('Any Title')
+    expect(inMemoryQuestionsRepository.items[0].id).toEqual(question.id)
+  })
 })
