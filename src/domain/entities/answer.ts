849f7f241144e0b2a305ae009999213f41dec6e4
FIllipe Romero <fillipe.romero@gmail.com>
2023-08-01 19:14:18 -0600

    Add getters & setters to the entities

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -28,4 +28,25 @@ export class Answer extends Entity<AnswerProps> {
   get content() { 
     return this.props.content 
   }
+
+  get createdAt() { 
+    return this.props.createdAt 
+  }
+
+  get updatedAt() { 
+    return this.props.updatedAt 
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat('...')
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
 }
