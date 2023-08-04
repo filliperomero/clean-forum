b3fd4f4834cda25997eabc6f85f810fe04bec334
FIllipe Romero <fillipe.romero@gmail.com>
2023-08-03 18:58:01 -0600

    Add comment on answer use case and answerComments repository

---
+++ src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -0,0 +1,35 @@
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { makeAnswer } from 'test/factories/make-answer'
+import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answer-comments-repository'
+import { CommentOnAnswerUseCase } from './comment-on-answer'
+
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: CommentOnAnswerUseCase // sut => System Under Test
+
+describe('Comment on Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository()
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    sut = new CommentOnAnswerUseCase(
+      inMemoryAnswersRepository,
+      inMemoryAnswerCommentsRepository,
+    )
+  })
+
+  it('should be able to comment on answer', async () => {
+    const answer = makeAnswer()
+
+    await inMemoryAnswersRepository.create(answer)
+
+    await sut.execute({
+      answerId: answer.id.toString(),
+      authorId: answer.authorId.toString(),
+      content: 'Comment on Answer',
+    })
+
+    expect(inMemoryAnswerCommentsRepository.items[0].content).toEqual(
+      'Comment on Answer',
+    )
+  })
+})
