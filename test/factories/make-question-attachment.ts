a2ba7490133f25af32508538d340e80f5f34df3f
FIllipe Romero <fillipe.romero@gmail.com>
2023-08-09 00:05:06 -0600

    Modify question to use QuestionAttachmentList and modify use cases related to it

---
+++ test/factories/make-question-attachment.ts
@@ -0,0 +1,21 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import {
+  QuestionAttachment,
+  QuestionAttachmentProps,
+} from '@/domain/forum/enterprise/entities/question-attachment'
+
+export function makeQuestionAttachment(
+  override: Partial<QuestionAttachmentProps> = {},
+  id?: UniqueEntityID,
+) {
+  const questionAttachment = QuestionAttachment.create(
+    {
+      questionId: new UniqueEntityID('question-id'),
+      attachmentId: new UniqueEntityID('attachment-id'),
+      ...override,
+    },
+    id,
+  )
+
+  return questionAttachment
+}
