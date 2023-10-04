9116e3b15db6ef23ff22b4f45e513cb8c93d8653
FIllipe Romero <fillipe.romero@gmail.com>
2023-10-03 22:09:33 -0600

    Add the use of the fetch recent questions use case and add question presenter

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -5,7 +5,10 @@ import { CreateAccountController } from './controllers/create-account.controller
 import { CreateQuestionController } from './controllers/create-question.controller'
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
 import { DatabaseModule } from '../database/prisma/database.module'
+
+// Use Cases
 import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question'
+import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
 
 @Module({
   imports: [DatabaseModule],
@@ -15,6 +18,6 @@ import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/crea
     CreateQuestionController,
     FetchRecentQuestionsController,
   ],
-  providers: [CreateQuestionUseCase],
+  providers: [CreateQuestionUseCase, FetchRecentQuestionsUseCase],
 })
 export class HttpModule {}
