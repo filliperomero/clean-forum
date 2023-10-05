69ab42e92bf0bb8366cfd406c5c51d37db0f070d
FIllipe Romero <fillipe.romero@gmail.com>
2023-10-04 23:51:41 -0600

    Refactor Authenticate controller to use the authenticate student use case

--- src/infra/database/prisma/database.module.ts
+++ src/infra/database/prisma/database.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma.service'
 import { PrismaQuestionsRepository } from './repositories/prisma-questions-repository'
+import { PrismaStudentsRepository } from './repositories/prisma-students-repository'
 
 // Interface/Abstract Class
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+import { StudentsRepository } from '@/domain/forum/application/repositories/students-repository'
 
 @Module({
   providers: [
@@ -12,7 +14,11 @@ import { QuestionsRepository } from '@/domain/forum/application/repositories/que
       provide: QuestionsRepository,
       useClass: PrismaQuestionsRepository,
     },
+    {
+      provide: StudentsRepository,
+      useClass: PrismaStudentsRepository,
+    },
   ],
-  exports: [PrismaService, QuestionsRepository], // Every module that imports DatabaseModule will have access to what we're exporting here
+  exports: [PrismaService, QuestionsRepository, StudentsRepository], // Every module that imports DatabaseModule will have access to what we're exporting here
 })
 export class DatabaseModule {}
