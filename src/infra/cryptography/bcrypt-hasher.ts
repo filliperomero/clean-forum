674e4570f229fe316b5ce8649675d10711e46d2d
FIllipe Romero <fillipe.romero@gmail.com>
2023-10-04 23:40:21 -0600

    Add cryptography Module

---
+++ src/infra/cryptography/bcrypt-hasher.ts
@@ -0,0 +1,16 @@
+import { compare, hash } from 'bcryptjs'
+
+import { HashComparer } from '@/domain/forum/application/cryptography/hash-comparer'
+import { HashGenerator } from '@/domain/forum/application/cryptography/hash-generator'
+
+export class BcryptHasher implements HashGenerator, HashComparer {
+  private HASH_SALT_LENGTH = 10
+
+  async hash(plain: string): Promise<string> {
+    return hash(plain, this.HASH_SALT_LENGTH)
+  }
+
+  async compare(plain: string, hash: string): Promise<boolean> {
+    return compare(plain, hash)
+  }
+}
