af891072067ba3a68df931827fe46d6e5cff267c
filliperomero <fillipe.romero@gmail.com>
2024-04-20 23:44:10 -0600

    Add delete question comment controller

---
+++ src/infra/http/controllers/delete-question-comment.controller.ts
@@ -0,0 +1,47 @@
+import {
+  BadRequestException,
+  Controller,
+  HttpCode,
+  Param,
+  NotFoundException,
+  ForbiddenException,
+  Delete,
+} from '@nestjs/common'
+import { CurrentUser } from '@/infra/auth/current-user-decorator'
+import { UserPayload } from '@/infra/auth/jwt.strategy'
+import { DeleteQuestionCommentUseCase } from '@/domain/forum/application/use-cases/delete-question-comment'
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
+
+@Controller('/questions/comments/:id')
+export class DeleteQuestionCommentController {
+  constructor(private deleteQuestionComment: DeleteQuestionCommentUseCase) {}
+
+  @Delete()
+  @HttpCode(204)
+  async handle(
+    @CurrentUser() user: UserPayload,
+    @Param('id') questionCommentId: string,
+  ) {
+    const { sub: userId } = user
+
+    const result = await this.deleteQuestionComment.execute({
+      questionCommentId,
+      authorId: userId,
+    })
+
+    if (result.isLeft()) {
+      const error = result.value
+      console.log(error)
+
+      switch (error.constructor) {
+        case ResourceNotFoundError:
+          throw new NotFoundException(error.message)
+        case NotAllowedError:
+          throw new ForbiddenException(error.message)
+        default:
+          throw new BadRequestException(error.message)
+      }
+    }
+  }
+}
